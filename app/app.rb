ce57b4d4ca689d876a5bbb86c760bb457ae43bb7
Alice Wyan <finitud@gmail.com>
2015-09-23 12:08:50 +0100

    Accept incoming POST data on /

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,35 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
+require 'json'
+
+require_relative './models/email_data'
 
 module Backend
   class App < Sinatra::Application
     register Sinatra::ActiveRecordExtension
 
     post  '/' do
+      payload = request.body.read
+
+      begin
+        json = JSON.parse(payload)
+      rescue JSON::ParserError
+        return 400
+      end
+
+      data = EmailData.new
+
+      begin
+        data.address = json.fetch('Address')
+        data.email_type = json.fetch('EmailType')
+        data.event = json.fetch('Event')
+        data.timestamp = json.fetch('Timestamp')
+      rescue KeyError
+        return 422
+      end
+
+      data.save!
+
       200
     end
 
--- spec/app/app_spec.rb
+++ spec/app/app_spec.rb
@@ -6,15 +6,24 @@ module Backend
       Backend::App
     end
 
-    describe 'endpoint /' do
-      it 'should accept incoming POST requests' do
-        post '/'
-        expect(last_response.status).to eq(200)
+    describe 'POST /' do
+      context 'with ill formed requests' do
+        it 'should return 422' do
+          post '/'
+          expect(last_response.status).to eq(422)
+        end
+      end
+
+      context 'with well formed requests' do
+        it 'should accept the request' do
+          post '/', "{}", "CONTENT_TYPE" => "application/json"
+          expect(last_response.status).to eq(200)
+        end
       end
     end
 
-    describe 'endpoint /stats' do
-      it 'should accept incoming GET requests' do
+    describe 'GET /stats' do
+      it 'should accept incoming requests' do
         get '/stats'
         expect(last_response.status).to eq(200)
       end
